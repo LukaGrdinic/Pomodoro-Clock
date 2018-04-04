7636a69fe3252c372da70105611274ab732df5ca
LukaGrdinic <1lukagrdinic@gmail.com>
2018-04-04 22:01:45 +0200

    html and css cleanup
    Cleaned up index.html and main.css.
    The app is basically done. Need to make small adjustments and improvements.
    Should add some features to pomodoro.js also

--- src/js/pomodoro.js
+++ src/js/pomodoro.js
@@ -1,7 +1,6 @@
-let workingTimeValue = parseInt($('#setWorkingTime').text());
-let breakTimeValue = parseInt($('#setBreakTime').text());
+/* EVENT HANDLERS */
 
-// Increase and Decrease workingTime
+// Increase and Decrease workingTimeSet
 $('#increaseWorkingTime').on('click', function () {
 	if (clock.timerStarted === false) {
 		clock.workingTimeSet++;
@@ -10,14 +9,14 @@ $('#increaseWorkingTime').on('click', function () {
 	}
 });
 $('#decreaseWorkingTime').on('click', function () {
-	if (clock.timerStarted === false) {
+	if (clock.timerStarted === false && clock.workingTimeSet > 1) {
 		clock.workingTimeSet--;
 		resetWorkAndBreakTime();
 		$('#setWorkingTime').text(clock.workingTimeSet);
 	}
 });
 
-// Increase and Decrease breakTime
+// Increase and Decrease breakTimeSet
 $('#increaseBreakTime').on('click', function () {
 	if (clock.timerStarted === false) {
 		clock.breakTimeSet++;
@@ -26,19 +25,22 @@ $('#increaseBreakTime').on('click', function () {
 	}
 });
 $('#decreaseBreakTime').on('click', function () {
-	if (clock.timerStarted === false) {
+	if (clock.timerStarted === false && clock.breakTimeSet > 1) {
 		clock.breakTimeSet--;
 		resetWorkAndBreakTime();
 		$('#setBreakTime').text(clock.breakTimeSet);
 	}
 });
 
+let workingTimeValue = parseInt($('#setWorkingTime').text());
+let breakTimeValue = parseInt($('#setBreakTime').text());
+
 let clock = {
 	timerStarted: false,
 	workingTimeSet: workingTimeValue, // This is the value of the #setWorkingTime Element
-	breakTimeSet: breakTimeValue, // This is the value of the #BreakTime Element
+	breakTimeSet: breakTimeValue, // This is the value of the #setBreakTime Element
 	timer: {
-		minutes: 25, // workingTimeSet should be assigned here
+		minutes: 25,
 		seconds: 0,
 		breakMinutes: 5,
 		breakSeconds: 0
@@ -67,12 +69,11 @@ function resetTimer() {
 }
 
 function countdown() {
-	if (clock.timer.minutes < 0) { // Start the breakTime countdown 
-		if (clock.timer.breakMinutes < 0) { // breakTime ends
+	if (clock.timer.minutes === 0 && clock.timer.seconds === 0) { // Start the breakTime countdown 
+		if (clock.timer.breakMinutes === 0 && clock.timer.breakSeconds === 1) { // when breakTime ends,adding one second because the next interval invokes countdown after 1 second
 			// Sound Signalization when break ends
-			// Reset the workingTime and breakTime before starting the workingTime countdown
-			resetWorkAndBreakTime();
-		} else { // breakTime running
+			resetWorkAndBreakTime(); // Reset the workingTime and breakTime before starting the workingTime countdown
+		} else { // while breakTime is running
 			// Sound Signalization when break starts
 			if (clock.timer.breakMinutes === clock.breakTimeSet && clock.timer.breakSeconds === 0) {
 				$('#timeLeftText').text('Break Time!');
@@ -84,6 +85,7 @@ function countdown() {
 			}
 			clock.timer.breakSeconds--;
 			console.log(clock.timer); // This is the movement of the hand on the clock
+			moveHand();
 		}
 	} else { // Start the workingTime countdown
 
@@ -94,6 +96,7 @@ function countdown() {
 		}
 		clock.timer.seconds--;
 		console.log(clock.timer); // This is the movement of the hand on the clock
+		moveHand();
 	}
 }
 
@@ -104,7 +107,28 @@ function resetWorkAndBreakTime() { // I should set other things in this function
 	clock.timer.breakSeconds = 0;
 	$('#timeLeftText').text('Time until break:');
 	$('#timeLeft').text(clock.timer.minutes);
+	moveHand(); // reset the hand on the clock
 }
+
+/* MOVEMENT OF THE HAND ON THE CLOCK */
+
+function moveHand() {
+	var observedSeconds;
+	if (clock.timer.minutes === 0 && clock.timer.seconds === 0) { 	// If breakTime is running
+		observedSeconds = clock.timer.breakSeconds;
+	} else { // If workingTime is running
+		observedSeconds = clock.timer.seconds;
+	}
+	var secondsTodegres = -(observedSeconds * 6); // 6 = 360deg/60s and turning it clockwise
+	$('.seconds').css('transform', `rotate(${secondsTodegres}deg)`); // using template literals - backticks ``
+}
+
+// MAKE SURE THERE ARE SOUND EFFECTS
+// ADD SOME ANIMATIONS TO #TIMELEFT ELEMENT (MAYBE TO #SETWORKINGTIME AND #SETBREAKTIME ALSO)
+// DO SOME REFACTORING (USE CLOSURE TRICKS IF POSSIBLE) - MAYBE DIVIDE FUNCTIONS IN OBJECTS, LIKE WITH JAVASCRIPT CALCULATOR
+// MAKE SURE THE MOVEMENT OF THE HAND IS A BIT SMOOTHER, IF POSSIBLE TO FIX WITH JAVASCRIPT
+// ADD SOME KEYCODE EVENTS, BY PRESSING ENTER, TOGGLE THE TOGGLETIMER FUNCTION
+// ADD A FEATURE - WHEN 10S BEFORE BREAK, SET SOME VISUAL AND/OR SOUND SIGNALIZATION
 
 // VALJALO BI DA NA KRAJU SVE UBACIM U JEDNU $document.ready funkciju
 // SREDITI STVARI SA JQUERRY-jem, AKO KORISTIM JQUERRY, DRZIM SE JQUERRYJA, NPR NEMA STAVLJANJA ONIH ONCLICKOVA U HTML-U
